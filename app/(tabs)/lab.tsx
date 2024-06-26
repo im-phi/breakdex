3905059f81e1851541a34a4722b3dfe78f2d1d20
Phi <pgh30@scarletmail.rutgers.edu>
2024-06-25 20:20:29 -0400

    first commit

---
+++ app/(tabs)/lab.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const lab = () => {
+  return (
+	<View>
+	  <Text>lab</Text>
+	</View>
+  )
+}
+
+export default lab
---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Redirect } from "expo-router";
+
+const StartPage = () => {
+  return <Redirect href="/library" />
+};
+
+export default StartPage
